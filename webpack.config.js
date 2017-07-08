929bf4c1119fc24fbc62a150d6e053b4801dc260
edocz <noarch@163.com>
2017-07-08 14:56:49 +0800

    format

--- webpack.config.js
+++ webpack.config.js
@@ -9,26 +9,24 @@ module.exports = {
 		path.resolve(ROOT_PATH,'app/src/index')
 	],
 	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				use: [{
-					loader: 'react-hot-loader',
-					loader: 'babel-loader'
-				}]
-			},
-			{
-				test: /\.scss$/,
-				use: [{
-						loader: "style-loader" // creates style nodes from JS strings
-				}, {
-						loader: "css-loader" // translates CSS into CommonJS
-				}, {
-						loader: "sass-loader" // compiles Sass to CSS
-				}]
-			}
-		]
+		rules: [{
+			test: /\.jsx?$/,
+			exclude: /node_modules/,
+			use: [{
+				loader: 'react-hot-loader',
+				loader: 'babel-loader'
+			}]
+		},
+		{
+			test: /\.scss$/,
+			use: [{
+					loader: "style-loader" // creates style nodes from JS strings
+			}, {
+					loader: "css-loader" // translates CSS into CommonJS
+			}, {
+					loader: "sass-loader" // compiles Sass to CSS
+			}]
+		}]
 	},
 	resolve: {
 		extensions: ['.js', '.jsx']
